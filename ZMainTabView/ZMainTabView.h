ffa7af41cbd80c94cb9c2bd19d031f680d283ce9
mohamedzead1996 <fci.scu.mohmadmahmoudzead@gmail.com>
2020-02-07 14:54:28 +0200

    initial project setup

---
+++ ZMainTabView/ZMainTabView.h
@@ -0,0 +1,19 @@
+//
+//  ZMainTabView.h
+//  ZMainTabView
+//
+//  Created by Mac on 2/7/20.
+//  Copyright © 2020 spark-cloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZMainTabView.
+FOUNDATION_EXPORT double ZMainTabViewVersionNumber;
+
+//! Project version string for ZMainTabView.
+FOUNDATION_EXPORT const unsigned char ZMainTabViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZMainTabView/PublicHeader.h>
+
+
